26d29dcfb05f1c2b9d26e184a5cac3854dc920e6
maruf <maruf@tryalpine.com>
2024-09-17 10:35:00 -0500

    Add base script

--- script/XUltraLRTBase.s.sol
+++ script/XUltraLRTBase.s.sol
@@ -13,9 +13,18 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
 contract XUltraLRTBase is Script {
-    address timelock = 0x535B06019dD972Cd48655F5838306dfF8E68d6FD; // blast mainnet
+    address timelock = 0x535B06019dD972Cd48655F5838306dfF8E68d6FD; // base mainnet
+    address multisig = 0x8ACbb784Aa852268343cD8e3BD2099477E0a2F63; // base multisig
     bool broadcastActive;
+
+    function _checkTimeLockMultiSig() internal {
+        // check multisig role
+        TimelockController tlc = TimelockController(payable(timelock));
+        require(tlc.hasRole(tlc.PROPOSER_ROLE(), multisig), "multisig not proposer");
+    }
 
     function _start() internal returns (address) {
         if (broadcastActive) {
@@ -27,6 +36,9 @@ contract XUltraLRTBase is Script {
         console2.log("deployer add %s", deployer);
         console2.log("deployer balance %s", deployer.balance);
         broadcastActive = true;
+
+        _checkTimeLockMultiSig();
+
         return deployer;
     }
 
@@ -55,7 +67,7 @@ contract XUltraLRTBase is Script {
 
         XERC20Factory factoryImpl = new XERC20Factory();
 
-        console2.log("factoryImpl %s", address(factoryImpl));
+        console2.log("factory Impl %s", address(factoryImpl));
 
         bytes memory initData = abi.encodeCall(XERC20Factory.initialize, (xErc20LockboxImpl, xUltraLRTImpl));
 
@@ -65,12 +77,12 @@ contract XUltraLRTBase is Script {
 
         XERC20Factory factory = XERC20Factory(address(proxy));
 
-        console2.log("factory %s", address(factory));
+        console2.log("factory add %s", address(factory));
 
         return address(factory);
     }
 
-    function deployUltraEthSBlast() public {
+    function deployUltraEthS() public {
         address deployer = _start();
         XERC20Factory factory = XERC20Factory(deployFactory());
 
@@ -85,5 +97,61 @@ contract XUltraLRTBase is Script {
         console2.log("name %s", XUltraLRT(payable(xErc20Addr)).name());
         console2.log("symbol %s", XUltraLRT(payable(xErc20Addr)).symbol());
         console2.log("owner %s", XUltraLRT(payable(xErc20Addr)).owner());
+    }
+
+    // generate tx data
+    function genInitMailboxPropData() public {
+        XUltraLRT lrt = new XUltraLRT();
+
+        address mailbox = 0x28EFBCadA00A7ed6772b3666F3898d276e88CAe3;
+
+        uint8 totalChain = 3;
+        uint32[] memory domains = new uint32[](totalChain);
+        address[] memory routers = new address[](totalChain);
+
+        // eth receiver
+        domains[0] = 1;
+        routers[0] = 0x91F822fAFc1db552e78f49941776aCB2a78fD422;
+
+        // blast
+        domains[1] = 81457;
+        routers[1] = 0xbb4e01B8940E8E2b3a95cED7941969D033786FF7;
+
+        // linea
+        domains[2] = 59144;
+        routers[2] = 0xB838Eb4F224c2454F2529213721500faf732bf4d;
+
+        bytes memory data = abi.encodeCall(XUltraLRT.initMailbox, (mailbox, domains, routers));
+        // bytes memory data = abi.encodeCall(XUltraLRT.setWithdrawalFeeBps,(10));
+
+        console2.logBytes(data);
+    }
+
+    function convAddTo32Bytes() public {
+        address addr = address(0x5217C8F3B7fb8B6501C8FF2a4C09b14B4B08C9f9);
+        bytes32 data = bytes32(uint256(uint160(addr)));
+        console2.logBytes32(data);
+    }
+
+    function readDecimals() public {
+        XUltraLRT lrt = XUltraLRT(payable(0x14Dc0EA777a87CAF54E49c9375B39727e1D85B69));
+        console2.log("decimals %s", lrt.owner());
+        uint256 codeSize;
+        assembly {
+            codeSize := extcodesize(0x14Dc0EA777a87CAF54E49c9375B39727e1D85B69)
+        }
+
+        console2.log("code size %s", codeSize);
+    }
+
+    function buildConsArgs() public {
+        address xUltraLRTImpl = 0x78Bb94Feab383ccEd39766a7d6CF31dED177Ad0c; // base mainnet
+        address xErc20LockboxImpl = 0x9BA3f0899E9272d85E6D380fc2C735b60EC5f4bB; // base mainnet
+        address factory = 0xFca8A394368e6d6096B34a043748Db30d7Bf97E7; // base mainnet
+
+        bytes memory initializeBytecode =
+            abi.encodeCall(XUltraLRT.initialize, ("Affine ultraETHs 2.0", "ultraETHs", timelock, factory));
+
+        console2.logBytes(initializeBytecode);
     }
 }
